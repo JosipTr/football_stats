db7034b802f1ddce8fb1d45c42ffb0c41a6181ae
JosipTr <jtr.other13@gmail.com>
2022-09-30 10:56:11 +0200

    Create PlayerRepository abstract class

---
+++ lib/domain/repositories/player_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:football_stats/data/models/player_model.dart';
+
+abstract class PlayerRepository {
+  Future<Either<Exception, List<Player>>> getPlayers();
+  Future<Either<Exception, void>> addPlayer(Player player);
+  Future<Either<Exception, void>> removePlayer(Player player);
+}
