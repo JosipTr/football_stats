065bd1a4d62a2a37a95b9297157216aec61dfe09
JosipTr <jtr.other13@gmail.com>
2022-09-30 11:18:48 +0200

    Create events

--- lib/presentation/bloc/player_event.dart
+++ lib/presentation/bloc/player_event.dart
@@ -1,3 +1,17 @@
 part of 'player_bloc.dart';
 
 abstract class PlayerEvent {}
+
+class GetPlayersEvent extends PlayerEvent{}
+
+class AddPlayerEvent extends PlayerEvent{
+  final Player player;
+
+  AddPlayerEvent(this.player);
+}
+
+class RemovePlayerEvent extends PlayerEvent {
+  final Player player;
+
+  RemovePlayerEvent(this.player);
+}
