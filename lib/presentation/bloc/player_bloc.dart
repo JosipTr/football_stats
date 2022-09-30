e3c8c244057d1ce4da779ff278ac36daeda50793
JosipTr <jtr.other13@gmail.com>
2022-09-30 11:31:32 +0200

    Implement Bloc logic

--- lib/presentation/bloc/player_bloc.dart
+++ lib/presentation/bloc/player_bloc.dart
@@ -1,12 +1,39 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:football_stats/data/models/player_model.dart';
+import 'package:football_stats/domain/usecases/add_player_usecase.dart';
+import 'package:football_stats/domain/usecases/get_players_usecase.dart';
+import 'package:football_stats/domain/usecases/remove_player_usecase.dart';
 
 part 'player_event.dart';
 part 'player_state.dart';
 
 class PlayerBloc extends Bloc<PlayerEvent, PlayerState> {
-  PlayerBloc() : super(PlayerInitial()) {
-    on<PlayerEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetPlayers _getPlayers;
+  final AddPlayer _addPlayer;
+  final RemovePlayer _removePlayer;
+
+  List<Player> players = List.empty(growable: true);
+
+  PlayerBloc(this._getPlayers, this._addPlayer, this._removePlayer) : super(PlayerInitial()) {
+    on<GetPlayersEvent>(_onGetPlayersEvent) {
+      
+    };
+  }
+
+  void _onGetPlayersEvent(GetPlayersEvent event, Emitter<PlayerState> emit) async{
+    var either = await _getPlayers.call();
+    either.fold((l) => Exception(), (r) => players = r);
+    emit(Loaded(players));
+  }
+
+  void _onAddPlayerEvent(AddPlayerEvent event, Emitter<PlayerState> emit) async{
+    var either = await _addPlayer.call(event.player);
+    either.fold((l) => Exception(), (r) => players.add(event.player));
+    emit(Loaded(players));
+  }
+  void _onRemovePlayerEvent(RemovePlayerEvent event, Emitter<PlayerState> emit) async{
+    var either = await _removePlayer.call(event.player);
+    either.fold((l) => Exception(), (r) => players.remove(event.player));
+    emit(Loaded(players));
   }
 }
