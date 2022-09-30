0783d385276a8bd2333ef6d592662fba1121f1f4
JosipTr <jtr.other13@gmail.com>
2022-09-30 11:20:12 +0200

    Create states

--- lib/presentation/bloc/player_state.dart
+++ lib/presentation/bloc/player_state.dart
@@ -3,3 +3,17 @@ part of 'player_bloc.dart';
 abstract class PlayerState {}
 
 class PlayerInitial extends PlayerState {}
+
+class Loading extends PlayerState {}
+
+class Loaded extends PlayerState{
+  final List<Player> players;
+
+  Loaded(this.players);
+}
+
+class Error extends PlayerState {
+  final String message;
+
+  Error(this.message);
+}
